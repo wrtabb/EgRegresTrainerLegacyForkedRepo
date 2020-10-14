546a651b008a2c521759a41e608b94de6a2e7bee
Robert Tabb <wrtabb@gmail.com>
2020-10-14 15:30:11 -0500

    made new branch

--- scripts/runSCRegTrainings.py
+++ scripts/runSCRegTrainings.py
@@ -12,16 +12,9 @@ def main():
 
     parser = argparse.ArgumentParser(description='runs the SC regression trainings')
     parser.add_argument('--era',required=True,help='year to produce for, 2016, 2017, 2018 are the options')
-    parser.add_argument('--input_dir','-i',default='/home/hep/wrtabb/Egamma/input_trees',help='input directory with the ntuples')
-    parser.add_argument('--output_dir','-o',default="results",help='output dir')
+    parser.add_argument('--input_dir','-i',default='../input_trees',help='input directory with the ntuples')
+    parser.add_argument('--output_dir','-o',default="../results",help='output dir')
     args = parser.parse_args()
-
-    #step 1, run calo only regression on the ideal IC to get the mean
-    #step 2, apply the mean to the real IC sample and save the result in a tree
-    #step 3, retrain the resolution for the real IC on the corrected energy
-    run_step1 = True
-    run_step2 = True
-    run_step3 = True
 
     #setup the selection (event number cuts come later)
     cuts_name = "stdCuts" 
@@ -42,8 +35,8 @@ def main():
         real_eventnr_cut = "evt.eventnr%2==0" #events in the ntuple are different so can get away with this
     elif args.era=='2018':
         base_reg_name = "scReg2018UL"    
-        input_ideal_ic  = "{}/DoubleElectron_FlatPt-1To300_2018ConditionsFlatPU0to70ECALGT_105X_upgrade2018_realistic_IdealEcalIC_v4-v1_AODSIM_EgRegTreeV5_partStatsV2.root".format(args.input_dir)
-        input_real_ic = "{}/DoubleElectron_FlatPt-1To300_2018ConditionsFlatPU0to70RAW_105X_upgrade2018_realistic_v4-v1_AODSIM_EgRegTreeV5_partStatsV2.root".format(args.input_dir)    
+        input_ideal_ic  = "{}/DoubleElectron_FlatPt-1To300_FlatPU0to70_106X_upgrade2018_realistic_v11_Ecal3-v4_AODSIM_EgRegTree/DoubleElectron_FlatPt-1To300_FlatPU0to70_106X_upgrade2018_realistic_v11_Ecal3-v4_AODSIM_EgRegTree.root".format(args.input_dir)
+        input_real_ic = "{}/DoubleElectron_FlatPt-1To300_FlatPU0to70_106X_upgrade2018_realistic_v11_Ecal3-v4_AODSIM_EgRegTree/DoubleElectron_FlatPt-1To300_FlatPU0to70_106X_upgrade2018_realistic_v11_Ecal3-v4_AODSIM_EgRegTree.root".format(args.input_dir)    
         ideal_eventnr_cut = "evt.eventnr%5==0"  #4million electrons
         real_eventnr_cut = "evt.eventnr%5==1" #4million electrons
     else:
@@ -83,18 +76,6 @@ steps to be run:
     forest_ee_file = regArgs.output_name()
 
     regArgs.base_name = "{}_RealIC_IdealTraining".format(base_reg_name)
-    input_for_res_training = str(regArgs.applied_name()) #save the output name before we change it
-    input_for_input_for_res_training = str(input_real_ic)
-    
-    if run_step2: subprocess.Popen(["bin/slc6_amd64_gcc700/RegressionApplierExe",input_for_input_for_res_training,input_for_res_training,"--gbrForestFileEE",forest_ee_file,"--gbrForestFileEB",forest_eb_file,"--nrThreads","4","--treeName",regArgs.tree_name,"--writeFullTree","1","--regOutTag","Ideal"]).communicate()
-
-    regArgs.base_name = "{}_RealIC_RealTraining".format(base_reg_name)
-    regArgs.input_training = input_for_res_training
-    regArgs.input_testing = input_for_res_training
-    regArgs.target = "mc.energy/(sc.rawEnergy*regIdealMean)"
-    regArgs.fix_mean = True
-    regArgs.cuts_base = base_ele_cuts.format(extra_cuts = real_eventnr_cut)
-    if run_step3: regArgs.run_eb_and_ee()
 
 if __name__ =='__main__':
     main()
